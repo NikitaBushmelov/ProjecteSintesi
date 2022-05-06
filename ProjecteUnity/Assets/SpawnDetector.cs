1fa5b535d1947a52d4a3bbe824c3ba50e6b6bb96
NikitaBushmelov <bushmelovnikita@gmail.com>
2022-05-06 12:34:47 +0200

    main

--- ProjecteUnity/Assets/SpawnDetector.cs
+++ ProjecteUnity/Assets/SpawnDetector.cs
@@ -5,11 +5,31 @@ using UnityEngine;
 public class SpawnDetector : MonoBehaviour
 {
     public bool spawn;
+    public bool spawned;
+    Transform[] allChildren;
+    bool clear;
+    private void Start()
+    {
+        allChildren = this.transform.GetComponentsInChildren<Transform>();
+        for (int i = 0; i < allChildren.Length; i++)
+        {
+            Debug.Log(allChildren[i].name);
+        }
+    }
+    private void Update()
+    {
+
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player")) {
+        if (collision.CompareTag("Player")&&spawned==false) {
             //GameObject.FindGameObjectWithTag("SpawnPoint").GetComponent<DoorSpawning>();
             spawn = true;
+            spawned = true;
+        }
+        if (collision.CompareTag("Enemy")) { 
+            
         }
     }
 }
