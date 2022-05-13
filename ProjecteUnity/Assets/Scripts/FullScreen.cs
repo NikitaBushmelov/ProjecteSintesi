d81a557766aee4aaf1a577dde18b93df70c3cdc9
Artur <arturcarrera7@gmail.com>
2022-05-13 10:48:43 +0200

    12

---
+++ ProjecteUnity/Assets/Scripts/FullScreen.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class FullScreen : MonoBehaviour
+{
+
+    public Toggle toggle;
+    public Dropdown resolucionesDropDown;
+    Resolution[] resoluciones; 
+
+
+
+    void Start()
+    {
+        if (Screen.fullScreen)
+        {
+           toggle.isOn =true;
+        }else{
+            toggle.isOn = false;
+        }
+        RevisarResolucion();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+    }
+    public void ActivarPantallaComleta(bool pantallaCompleta)
+    {
+        Screen.fullScreen = pantallaCompleta;
+    }
+
+    public void RevisarResolucion ()
+    {
+        resoluciones = Screen.resolutions;
+        resolucionesDropDown.ClearOptions();
+        List<string> opciones = new List<string>();
+        int resolucionActual = 0;
+        for (int i = 0; i < resoluciones.Length; i++)
+        {
+            string opcion = resoluciones[i].width + " x " + resoluciones[i].height;
+            opciones.Add(opcion);
+
+            if (Screen.fullScreen && resoluciones[i].width == Screen.currentResolution.width &&
+            resoluciones[i].height == Screen.currentResolution.height)
+            {
+                resolucionActual = i;
+
+            }
+
+        }
+        resolucionesDropDown.AddOptions(opciones);
+        resolucionesDropDown.value = resolucionActual;
+        resolucionesDropDown.RefreshShownValue();
+
+        // resolucionesDropDown.value = PlayerPrefs.GetInt("numeroResolucion",0);
+
+    }
+    public void CambiarResolution (int indiceResolucion)
+    {
+       // PlayerPrefs.SetInt("numeroResolucion", resolucionesDropDown.value);
+
+
+        Resolution resolucion = resoluciones[indiceResolucion];
+        Screen.SetResolution(resolucion.width, resolucion.height, Screen.fullScreen);
+
+
+    }
+}
