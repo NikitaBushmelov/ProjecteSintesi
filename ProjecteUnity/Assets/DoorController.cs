d3464d0b8bd85e1d50960b901f40f7fd1cbfee4a
Artur <arturcarrera7@gmail.com>
2022-05-23 11:26:33 +0200

    34

--- ProjecteUnity/Assets/DoorController.cs
+++ ProjecteUnity/Assets/DoorController.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 public class DoorController : MonoBehaviour
 {
 public GameObject porta;
-
+/*
 public void OnTriggerEnter2D (Collider2D other)
 {
  if (other.CompareTag("Player"))
         {
             porta.SetActive(true);
         }
-
+  
 }
 public void OnTriggerExit2D (Collider2D other)
 
@@ -22,7 +22,24 @@ public void OnTriggerExit2D (Collider2D other)
             porta.SetActive(false);
         }
 
-}
+}*/
+ void Update()
+    {
+        GameObject[] gameObjects;
+        gameObjects = GameObject.FindGameObjectsWithTag("enemy");
+
+        if (gameObjects.Length == 0 )
+        {
+            Debug.Log("No game objects are tagged with 'enemy'");
+            porta.SetActive(false);
+
+        }if(gameObjects.Length >= 1) {
+
+             Debug.Log("hay enemigos");
+            porta.SetActive(true);
+        }
+
+    }
 
 
 
--- ProjecteUnity/Assets/Scripts/Enemy.cs
+++ ProjecteUnity/Assets/Scripts/Enemy.cs
@@ -10,6 +10,11 @@ public class Enemy : MonoBehaviour
     private float rotationSpeed;
     public int Health = 100;
     public GameObject item;
+    public GameObject item1;
+    public GameObject item2;
+    public GameObject item3;
+    public GameObject item4;
+   
 
     public GameObject deathEffect;
     public float speed;
@@ -64,9 +69,15 @@ public class Enemy : MonoBehaviour
         int rand = Random.Range(0, 10);
         if (rand == 1)
         {
-
-            Instantiate(item, transform.position, Quaternion.identity);
-
+            Instantiate(item, transform.position, Quaternion.identity); 
+        }if (rand ==2){
+               Instantiate(item1, transform.position, Quaternion.identity);
+        }if (rand ==7){
+               Instantiate(item2, transform.position, Quaternion.identity);
+        }if (rand ==8){
+               Instantiate(item3, transform.position, Quaternion.identity);
+        }if (rand ==9){
+               Instantiate(item4, transform.position, Quaternion.identity);
         }
         Debug.Log("el randm " + rand);
 
